13dcb3b2612f9f9c8a14598cc3cb5cc0d9f2b5e2
qiyu <qiyu@tantanapp.com>
2021-07-23 18:53:30 +0800

    First commit

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewA: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewA = UIView(frame: CGRect(x: 0, y: 100, width: 40, height: 40))
+        viewA.backgroundColor = .green
+        view.addSubview(viewA)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 1, delay: 0, options: [.repeat, .autoreverse]) {
+            let newFrame = self.viewA.frame.offsetBy(dx: 350, dy: 0)
+            self.viewA.frame = newFrame
+        }
     }
 
 
